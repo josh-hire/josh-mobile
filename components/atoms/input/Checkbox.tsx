5c04be4654b63d92daccca50fd658f888c17ff5a
dhifanrazaqa <dhifanofficial@gmail.com>
2024-09-26 09:10:18 +0700

    add checkbox component

---
+++ components/atoms/input/Checkbox.tsx
@@ -0,0 +1,107 @@
+import { Colors } from "@/constants/Colors";
+import React, { useState } from "react";
+import {
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Image,
+} from "react-native";
+import { HeadingText } from "../text/HeadingText";
+
+interface CheckboxListProps {
+  data: string[];
+  onSelectionChange: (selectedItems: string[]) => void;
+  numColumns?: number;
+}
+
+export default function CheckboxList({
+  data,
+  onSelectionChange,
+  numColumns = 2,
+}: CheckboxListProps) {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+  const handleSelect = (item: string) => {
+    let updatedSelectedItems = [...selectedItems];
+
+    if (selectedItems.includes(item)) {
+      updatedSelectedItems = updatedSelectedItems.filter((i) => i !== item);
+    } else {
+      updatedSelectedItems.push(item);
+    }
+
+    setSelectedItems(updatedSelectedItems);
+    onSelectionChange(updatedSelectedItems);
+  };
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          style={styles.checkboxContainer}
+          onPress={() => handleSelect(item)}
+        >
+          <View
+            style={
+              selectedItems.includes(item)
+                ? styles.checkboxChecked
+                : styles.checkbox
+            }
+          >
+            {selectedItems.includes(item) && (
+              <View style={styles.checked}>
+                <Image
+                  source={require("../../../assets/icons/check-01.png")}
+                ></Image>
+              </View>
+            )}
+          </View>
+          <HeadingText type="h6" style={styles.label}>
+            {item}
+          </HeadingText>
+        </TouchableOpacity>
+      )}
+      numColumns={numColumns} 
+      columnWrapperStyle={numColumns > 1 ? styles.columnWrapper : null}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    flex: 1,
+  },
+  checkbox: {
+    width: 18,
+    height: 18,
+    borderWidth: 2,
+    borderColor: Colors.neutral.n09,
+    marginRight: 10,
+  },
+  checkboxChecked: {
+    width: 18,
+    height: 18,
+    borderColor: Colors.neutral.n09,
+    marginRight: 10,
+  },
+  checked: {
+    width: 18,
+    height: 18,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.secondary.s05,
+  },
+  label: {
+    fontSize: 16,
+    color: "#000",
+  },
+  columnWrapper: {
+    justifyContent: "space-between",
+  },
+});
