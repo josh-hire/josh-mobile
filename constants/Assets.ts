0e07c9ab2b3bfb45a0b6c4ac87d1b7f48bc11b4b
dhifanrazaqa <dhifanofficial@gmail.com>
2024-09-30 15:28:04 +0700

    add assets as constant

---
+++ constants/Assets.ts
@@ -0,0 +1,16 @@
+export const Assets = {
+  icons: {
+    plus01: require("@assets/icons/plus-01.png"),
+    check01: require("@assets/icons/check-01.png"),
+    chevronUp: require("@assets/icons/chevron-up.png"),
+    chevronDown: require("@assets/icons/chevron-down.png"),
+    x02: require("@assets/icons/x-02.png"),
+  },
+  logos: {},
+  illustrations: {
+    notFound: require("@assets/illustration/not-found.png"),
+  },
+  fonts: {
+    telkomselBatik: require("@assets/fonts/TelkomselBatikSans-Regular.otf"),
+  },
+};
