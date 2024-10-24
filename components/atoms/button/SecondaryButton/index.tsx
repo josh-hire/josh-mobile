8fbe9707f458b22581bd106c2efc45f2d00c1f34
dhifanrazaqa <dhifanofficial@gmail.com>
2024-10-24 09:40:54 +0700

    update secondary button icon component

--- components/atoms/button/SecondaryButton/index.tsx
+++ components/atoms/button/SecondaryButton/index.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps, Image, TouchableOpacity } from "react-native";
+import { ButtonProps, Image, TouchableOpacity, View } from "react-native";
 import { HeadingText } from "../../text/HeadingText";
 import { Assets } from "@constants/Assets";
 import styles from "@components/atoms/button/SecondaryButton/secondaryButton.styles";
@@ -6,16 +6,39 @@ import styles from "@components/atoms/button/SecondaryButton/secondaryButton.sty
 export type SecondaryButtonProps = ButtonProps & {
   title: string;
   handler: () => void;
+  color?: string;
+  position?: string;
 };
 
 export default function SecondaryButton({
   title,
   handler,
+  color = "black",
+  position = "left",
 }: SecondaryButtonProps) {
   return (
-    <TouchableOpacity style={styles.secondaryButton} onPress={handler}>
-      <Image source={Assets.icons.plus01}></Image>
-      <HeadingText type="h5">{title}</HeadingText>
+    <TouchableOpacity style={styles(color).secondaryButton} onPress={handler}>
+      {position === "left" ? (
+        <Image
+          source={
+            color === "black" ? Assets.icons.plus01 : Assets.icons.plus01White
+          }
+        ></Image>
+      ) : (
+        <View></View>
+      )}
+      <HeadingText type="h5" color={color}>
+        {title}
+      </HeadingText>
+      {position === "right" ? (
+        <Image
+          source={
+            color === "black" ? Assets.icons.plus01 : Assets.icons.plus01White
+          }
+        ></Image>
+      ) : (
+        <View></View>
+      )}
     </TouchableOpacity>
   );
 }
--- components/atoms/button/SecondaryButton/secondaryButton.styles.ts
+++ components/atoms/button/SecondaryButton/secondaryButton.styles.ts
@@ -1,13 +1,17 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  secondaryButton: {
-    borderRadius: 8,
-    borderWidth: 1,
-    padding: 12,
-    gap: 12,
-    flexDirection: "row",
-  },
-});
+const styles = (color: string) =>
+  StyleSheet.create({
+    secondaryButton: {
+      borderRadius: 8,
+      borderWidth: 1,
+      borderColor: color,
+      color: color,
+      padding: 12,
+      gap: 12,
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+  });
 
 export default styles;
