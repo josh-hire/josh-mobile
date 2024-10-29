bd5b08cefe8dfddd2499f574b9f12d1fcf3fdab0
dhifanrazaqa <dhifanofficial@gmail.com>
2024-10-29 11:38:02 +0700

    add button disabled mode

--- components/atoms/button/PrimaryButton/index.tsx
+++ components/atoms/button/PrimaryButton/index.tsx
@@ -7,17 +7,23 @@ export type PrimaryButtonProps = ButtonProps & {
   title: string;
   handler: () => void;
   color?: string;
+  isDisabled?: boolean;
 };
 
 export function PrimaryButton({
   title,
   handler,
   color = Colors.primary.p05,
+  isDisabled = false,
 }: PrimaryButtonProps) {
   return (
     <TouchableOpacity
-      style={[styles.buttonPrimary, { backgroundColor: color }]}
+      style={[
+        styles.buttonPrimary,
+        { backgroundColor: isDisabled ? Colors.neutral.n02 : color },
+      ]}
       onPress={handler}
+      disabled={isDisabled}
     >
       <HeadingText type="h5" color="white" fontWeight="bold">
         {title}
