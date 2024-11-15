3b4dc79842cf6b4771979062ebdce9d006da9fbf
dhifanrazaqa <dhifanofficial@gmail.com>
2024-11-15 15:37:45 +0700

    feat: add chat info card component

---
+++ components/molecules/card/chatInfoCard/chatInfoCard.styles.ts
@@ -0,0 +1,12 @@
+import { Colors } from "@constants/Colors";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  infoContainer: {
+    backgroundColor: Colors.neutral.n01,
+    padding: 12,
+    borderRadius: 5,
+  },
+});
+
+export default styles;
